d590299d104dc6f8b939d3a57953e1f5e15af84a
HawerPL <hubertsob217@gmail.com>
2025-10-04 18:30:06 +0200

    Dodaj pliki projektów.

---
+++ Models/Rcb.cs
@@ -0,0 +1,11 @@
+﻿namespace HackYeah_Backend.Models
+{
+    public class Rcb
+    {
+        public int Id { get; set; }
+        public string? Title { get; set; }
+        public string?  Description { get; set; }
+        public DateTime EventDate { get; set; }
+        public string? ImageUrl { get; set; }
+    }
+}
