20bca0470304737eae8c47294a264df674d5791c
10MiA01 <miklashevichanastasiyachelovek@gmail.com>
2025-10-05 08:37:24 +0200

    JSTNumber table + controller

---
+++ Controllers/JSTNumberController.cs
@@ -0,0 +1,32 @@
+﻿using HackYeah_Backend.Data;
+using HackYeah_Backend.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace HackYeah_Backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class JSTNumberController : Controller
+    {
+        private readonly ApplicationDbContext _dbContext;
+        public JSTNumberController(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        // GET: api/jstnumber/{location}
+        [HttpGet("{location}")]
+        public async Task<ActionResult<IEnumerable<JSTNumber>>> Get(string location)
+        {
+            var numbers = await _dbContext.jSTNumbers
+                .Where(l => l.Location == location)
+                .ToListAsync();
+
+            if (numbers == null || numbers.Count == 0)
+                return NotFound($"Brak danych dla miejscowości: {location}");
+
+            return Ok(numbers);
+        }
+    }
+}
---
+++ Models/JSTNumber.cs
@@ -0,0 +1,9 @@
+﻿namespace HackYeah_Backend.Models
+{
+    public class JSTNumber
+    {
+        public int Id { get; set; }
+        public string Location { get; set; }
+        public string Number { get; set; }
+    }
+}
